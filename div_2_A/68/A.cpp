be3b95a210e72015c09236380d645b98144f733b
mr-anon <ayush.goyal@research.iiit.ac.in>
2021-09-26 19:20:38 +0530

    800

---
+++ div_2_A/68/A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int t, n, x;
+    cin >> t;
+    for (int i = 0; i < t; ++i)
+    {
+    cin >> n;
+    cin >> x;
+    cout << x*2 << endl;
+
+    }
+}
