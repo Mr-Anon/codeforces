ad256e0416d89814dc813964913272233b3cbc97
mr-anon <ayush.goyal@research.iiit.ac.in>
2021-10-06 00:30:29 +0530

    800 - windows

--- div_2_A/57/A.cpp
+++ div_2_A/57/A.cpp
@@ -6,28 +6,14 @@ using namespace std;
 
 int main()
 {
-    int t,  n;
+    int t, l,r;
     cin >> t;
     for (int i = 0; i < t; ++i)
     {
-    cin >> n;
-    string s;
-    cin >> s;
-    int flag = 0; 
-    for(int j = 0; j < n-11; ++j){
-        if(s[j] == '8'){
-            flag = 1;
-        }
-    }
-    if(s[n-11] == '8'){
-            flag = 1;
-        }
-    if( flag == 0){
-        cout << "NO" << endl;
-    }
-    else{
-        cout << "YES" << endl;
-    }
+    cin >> l;
+    cin >> r;
+    cout << l << " ";
+    cout << l*2 << endl;
    
     }
 }
